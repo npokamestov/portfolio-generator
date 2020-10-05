db87fc622e4a57a2fbcc1682c2e0a42c58c43c2b
Cody Wyoming <68657852+npokamestov@users.noreply.github.com>
2020-10-05 13:09:35 -0400

    added confirmations

--- app.js
+++ app.js
@@ -74,17 +74,49 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: "What is your name?"
+            message: "What is your name? (Required)" ,
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your GitHub Username!');
+                    return false;
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for ab "About" section?',
+            default: true
         },
         {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
         }
     ]);
 };
@@ -108,7 +140,16 @@ Add a New Project
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter a description!');
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -119,7 +160,16 @@ Add a New Project
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                }
+                else {
+                    console.log('Please enter your project GitHub Link!');
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
